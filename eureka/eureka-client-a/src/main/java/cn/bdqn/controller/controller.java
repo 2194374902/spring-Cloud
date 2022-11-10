54e722b36030234f6dbaa6b9c7e0e0107a7d9403
2194374902 <102497715+2194374902@users.noreply.github.com>
2022-11-10 19:31:53 +0800

    第一次修改

--- eureka/eureka-client-a/src/main/java/cn/bdqn/controller/controller.java
+++ eureka/eureka-client-a/src/main/java/cn/bdqn/controller/controller.java
@@ -3,5 +3,6 @@ package cn.bdqn.controller;
 public class controller {
     public static void main(String[] args) {
         System.out.println("asdf");
+        System.out.println("二次修改");
     }
 }

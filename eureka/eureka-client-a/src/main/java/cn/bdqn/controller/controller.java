a801f91a96c6851f4015313bf6f1f50df70a88d7
2194374902 <102497715+2194374902@users.noreply.github.com>
2022-11-13 15:54:28 +0800

    集群设置和服务发现

--- eureka/eureka-client-a/src/main/java/cn/bdqn/controller/controller.java
+++ eureka/eureka-client-a/src/main/java/cn/bdqn/controller/controller.java
@@ -1,6 +1,45 @@
 package cn.bdqn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
 public class controller {
+    @Autowired
+    private DiscoveryClient discoveryClient; //服务发现
+
+
+    @GetMapping("text")
+    @ResponseBody
+    public String text(String name){
+        List<ServiceInstance> instances = discoveryClient.getInstances(name);
+        ServiceInstance serviceInstance=instances.get(0);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        System.out.println(host+port);
+
+
+        return instances.get(0).toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         System.out.println("asdf");
         System.out.println("二次修改");
